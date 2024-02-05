171612a07bb3734ab3a561b6139407b68827e554
Khawla <96916652+khawla-14@users.noreply.github.com>
2024-02-05 10:36:48 +0100

    IDK ZHQT I DID

--- src/components/BodyContainer/project/project.js
+++ src/components/BodyContainer/project/project.js
@@ -4,33 +4,33 @@ import './project.css';
 
 const Project = (props) => {
     const projects = props.projects;
-  return (
+    return (
 
-            <div className="projects-card">
-            
-                {projects.map((project) => (
-                        <div className='project-card' key={projects.id}>
-                            <img src={project.image} alt={`${project.title} Image`} className="project-image" />
-                            <div className='project-text-side'> 
-                            <h1 className="pproject-title">
-                                {project.title}
-                            </h1>
-                            <h4 className="clien-name">
-                                {project.clien}
-                            </h4>
-                            <p className="project-description">
-                                {project.description}
-                            </p>
-                            </div>
-                        </div>
-                   ) )}
-                
-                
-            
-            </div>
+        <div className="projects-card">
+
+            {projects.map((project) => (
+                <div className='project-card' key={projects.id}>
+                    <img src={project.image} alt={`${project.title} Image`} className="project-image" />
+                    <div className='project-text-side'>
+                        <h1 className="pproject-title">
+                            {project.title}
+                        </h1>
+                        <h4 className="clien-name">
+                            {project.clien}
+                        </h4>
+                        <p className="project-description">
+                            {project.description}
+                        </p>
+                    </div>
+                </div>
+            ))}
 
 
-  )
+
+        </div>
+
+
+    )
 }
 
 export default Project
