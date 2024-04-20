125ad4a2b5473545e00fcd495776b3426c55a8ea
Khawla <96916652+khawla-14@users.noreply.github.com>
2024-04-20 18:39:29 +0200

    projects update

--- src/components/BodyContainer/project/project.js
+++ src/components/BodyContainer/project/project.js
@@ -3,27 +3,43 @@ import './project.css';
 
 
 const Project = () => {
-  return (
+    return (
 
-            <div className="ag-courses_item">
+        <div className="ag-courses_item">
+
             <a href="#" className="ag-courses-item_link">
                 <div className="ag-courses-item_bg"></div>
 
                 <div className="ag-courses-item_title">
-                    UI/Web&amp;Graph design for teenagers 11-17&#160;years old
+                    Deveoped the Fund Landing page for "iradaa Arabia"
+                    in :
                 </div>
 
                 <div className="ag-courses-item_date-box">
-                     Start:
-                <span className="ag-courses-item_date">
-                    04.11.2022
-                </span>
+                    <span className="ag-courses-item_date">
+                        2022
+                    </span>
                 </div>
             </a>
-            </div>
+
+            <a href="#" className="ag-courses-item_link" >
+                <div className="ag-courses-item_bg"></div>
+
+                <div className="ag-courses-item_title">
+                    Deveoped & designed the "i-startec" start-up platform
+                    in :
+                </div>
+
+                <div className="ag-courses-item_date-box">
+                    <span className="ag-courses-item_date">
+                        2023
+                    </span>
+                </div>
+            </a>
+        </div>
 
 
-  )
+    )
 }
 
 export default Project
